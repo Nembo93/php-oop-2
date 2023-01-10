babd8599c9d4302811c617fe4581aa5a2ef2c8e3
nembo93 <francesco.c193@libero.it>
2023-01-10 16:42:40 +0100

    Aggiunge argomenti image e  type a classe Products

--- classes/foods.php
+++ classes/foods.php
@@ -1,12 +1,16 @@
 <?php
 class Food extends Products{
     public $name;
+    public $image;
+    public $type;
     public $price;
     public $ingredients;
 
-    public function __construct($name, $price, $ingredients)
+    public function __construct($name, $image, $type, $price, $ingredients)
     {
         $this->name = $name;
+        $this->image = $image;
+        $this->type = $type;
         $this->price = $price;
         $this->ingredients = $ingredients;
     }
@@ -14,12 +18,12 @@ class Food extends Products{
 }
 
 $food= [
-    new Food("Crocchette Meow", 5, "ciao"),
-    new Food("Manzo Meow", 3, "ciao"),
-    new Food("Salmone Meow", 3, "ciao"),
-    new Food("Crocchette Bau", 7, "ciao"),
-    new Food("Pollo Bau", 3, "ciao"),
-    new Food("Manzo e verdure Bau", 4, "ciao"),
+    new Food("Crocchette Meow", "img", "Food", 5, "Carne, verdure"),
+    new Food("Manzo Meow", "img", "Food", 3, "Manzo, verdure"),
+    new Food("Salmone Meow", "img", "Food", 3, "Salmone, verdure"),
+    new Food("Crocchette Bau", "img", "Food", 7, "Carne"),
+    new Food("Pollo Bau", "img", "Food", 3, "Food", "Pollo, verdure"),
+    new Food("Manzo e verdure Bau", "img", "Food", 4, "Manzo, verdure"),
 ]
 
 ?>
--- classes/products.php
+++ classes/products.php
@@ -2,11 +2,15 @@
 require_once __DIR__ ."/foods.php";
 class Products{
     public $name;
+    public $image;
+    public $type;
     public $price;
 
-    public function __construct(string $name, int $price)
+    public function __construct(string $name, string $type, string $image, int $price)
     {
         $this->name = $name;
+        $this->image = $image;
+        $this->type = $type;
         $this->price = $price;       
     }
 }
