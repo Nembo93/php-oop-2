91b37aa647828cd224ff478b853f1a6a65812025
nembo93 <francesco.c193@libero.it>
2023-01-10 16:26:18 +0100

    Aggiunge argomento(Ingredients) a classe Food

--- classes/foods.php
+++ classes/foods.php
@@ -1,18 +1,25 @@
 <?php
 class Food extends Products{
-    // public $name;
-    // public $price;
+    public $name;
+    public $price;
     public $ingredients;
+
+    public function __construct($name, $price, $ingredients)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->ingredients = $ingredients;
+    }
 
 }
 
 $food= [
-    new Food("Crocchette Meow", 5),
-    new Food("Manzo Meow", 3),
-    new Food("Salmone Meow", 3),
-    new Food("Crocchette Bau", 7),
-    new Food("Pollo Bau", 3),
-    new Food("Manzo e verdure Bau", 4),
+    new Food("Crocchette Meow", 5, "ciao"),
+    new Food("Manzo Meow", 3, "ciao"),
+    new Food("Salmone Meow", 3, "ciao"),
+    new Food("Crocchette Bau", 7, "ciao"),
+    new Food("Pollo Bau", 3, "ciao"),
+    new Food("Manzo e verdure Bau", 4, "ciao"),
 ]
 
 ?>
