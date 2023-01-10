f4aa7e29f19d1a3105899280cebc3c4d152646b0
nembo93 <francesco.c193@libero.it>
2023-01-10 18:53:03 +0100

    Ripete operazione precedente su classe Shelters

--- classes/Shelters.php
+++ classes/Shelters.php
@@ -1,15 +1,19 @@
 <?php
+require_once __DIR__ ."/Categories.php";
+
 class Shelters extends Products{
     public $name;
     public $image;
+    public $category;
     public $type;
     public $price;
     public $size;
 
-    public function __construct($name, $image, $type, $price, $size)
+    public function __construct($name, $image, Categories $category, $type, $price, $size)
     {
         $this->name = $name;
         $this->image = $image;
+        $this->category = $category;
         $this->type = $type;
         $this->price = $price;
         $this->size = $size;
@@ -17,11 +21,11 @@ class Shelters extends Products{
 }
 
 $shelters = [
-    new Shelters ("Mini bed Meow", "img", "Shelter", 10, "Small"),
-    new Shelters("Mini house Meow", "img", "Shelter", 18, "Small"),
-    new Shelters("Mini bed Bau", "img", "Shelter", 10, "Small"),
-    new Shelters("Bed Bau", "img", "Shelter", 15, "Regular"),
-    new Shelters("Mini house Bau", "Shelter", "Toy", 18, "Small"),
-    new Shelters("House Bau", "img", "Shelter", 30, "Regular"),
+    new Shelters ("Mini bed Meow", "img", new Categories ("Cat"), "Shelter", 10, "Small"),
+    new Shelters("Mini house Meow", "img", new Categories ("Cat"), "Shelter", 18, "Small"),
+    new Shelters("Mini bed Bau", "img", new Categories ("Dog"), "Shelter", 10, "Small"),
+    new Shelters("Bed Bau", "img", new Categories ("Dog"), "Shelter", 15, "Regular"),
+    new Shelters("Mini house Bau", "img", new Categories ("Dog"),"Shelter", "Toy", 18, "Small"),
+    new Shelters("House Bau", "img", new Categories ("Dog"), "Shelter", 30, "Regular"),
 ]
 ?>
