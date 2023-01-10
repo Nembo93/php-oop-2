98c6c2a4e7cd1bb8b0c1c0716f4fa9ce844e9514
nembo93 <francesco.c193@libero.it>
2023-01-10 15:57:20 +0100

    Aggiunge ereditarietà a classe Food

--- classes/foods.php
+++ classes/foods.php
@@ -1,10 +1,17 @@
 <?php
-$food = [
-    new Products("Crocchette Meow", 5),
-    new Products("Manzo Meow", 3),
-    new Products("Salmone Meow", 3),
-    new Products("Crocchette Bau", 7),
-    new Products("Pollo Bau", 3),
-    new Products("Manzo e verdure Bau", 4),
+class Food extends Products{
+    public $name;
+    public $price;
+    public $ingredients;
+}
+
+$food= [
+    new Food("Crocchette Meow", 5),
+    new Food("Manzo Meow", 3),
+    new Food("Salmone Meow", 3),
+    new Food("Crocchette Bau", 7),
+    new Food("Pollo Bau", 3),
+    new Food("Manzo e verdure Bau", 4),
 ]
+
 ?>
