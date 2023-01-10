03535f3c397febdf9168a779b6ee647bf4194778
nembo93 <francesco.c193@libero.it>
2023-01-10 15:42:46 +0100

    Aggiunge array di oggetti a classe foods

--- classes/foods.php
+++ classes/foods.php
@@ -1,5 +1,10 @@
 <?php
-$food = new Products();
-$food->name = "Meed";
-$food->price = 10;
+$food = [
+    new Products("Crocchette Meow", 5),
+    new Products("Manzo Meow", 3),
+    new Products("Salmone Meow", 3),
+    new Products("Crocchette Bau", 7),
+    new Products("Pollo Bau", 3),
+    new Products("Manzo e verdure Bau", 4),
+]
 ?>
--- classes/products.php
+++ classes/products.php
@@ -3,5 +3,11 @@ require_once __DIR__ ."/foods.php";
 class Products{
     public $name;
     public $price;
+
+    public function __construct(string $name, int $price)
+    {
+        $this->name = $name;
+        $this->price = $price;       
+    }
 }
 ?>
